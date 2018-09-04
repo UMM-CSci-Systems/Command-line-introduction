e6fda21a313e680f560d9ab192277df0b82481ee
bella148 <bella148@morris.umn.edu>
2018-09-04 11:22:57 -0500

    Add comments to script 1

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# Compress the file 'NthPrime.tgz' to the tmp directory provided in argument 2
 tar -xf NthPrime.tgz -C $2
 
+# Go to the tmp directory provided
 cd $2/NthPrime
 
+# Compile the contents of the tmp directory into a executable
 gcc main.c nth_prime.c -o NthPrime
 
+# Call the resulting program with our original number argument
 ./NthPrime $1
 
 
